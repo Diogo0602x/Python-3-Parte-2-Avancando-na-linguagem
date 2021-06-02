b6c57ad02f46383541293da161ea47acfa395b0a
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-02 11:53:03 -0300

    Aula 01 - Atividade 05 terminada

--- forca.py
+++ forca.py
@@ -1,9 +1,19 @@
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
+  print("*********************************")
+  print("***Bem vindo ao jogo da Forca!***")
+  print("*********************************")
 
-    print("Fim do jogo")
+  palavra_secreta = "banana"
+
+  enforcou = False
+  acertou = False
+
+  #enquanto(True E True)
+
+  while(nao enforcou E nao acertou):
+    print("jogando ...")
+
+  print("Fim do jogo")
 
 if(__name__ == "__main__"):
-    jogar()
+  jogar()
