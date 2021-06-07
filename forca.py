3078e25ddcf6419f3a1ee2b644c5ada933326c5b
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-07 09:32:19 -0300

    Aula 04 - Atividade 6 terminada

--- forca.py
+++ forca.py
@@ -1,31 +1,30 @@
 def jogar():
-  print("*********************************")
-  print("***Bem vindo ao jogo da Forca!***")
-  print("*********************************")
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-  palavra_secreta = "banana"
-  letras_acertadas = ["_", "_", "_", "_", "_", "_" ]
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
-  enforcou = False
-  acertou = False
-
-  print(letras_acertadas)
-
-  while(not enforcou and not acertou):
-
-    chute = input("Qual letra? ")
-    chute = chute.strip()
-
-    index = 0
-
-    for letra in palavra_secreta:
-      if(chute.upper() == letra.upper()):
-        letras_acertadas[index] = letra
-      index = index + 1
+    enforcou = False
+    acertou = False
 
     print(letras_acertadas)
 
-  print("Fim do jogo")
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
+
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
-  jogar()
+    jogar()
