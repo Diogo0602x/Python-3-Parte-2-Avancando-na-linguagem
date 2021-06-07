41d27bded2e941f2582706cd31caa1002aa186f1
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-07 08:38:50 -0300

    Aula 02 - Atividade 1 terminada

--- forca.py
+++ forca.py
@@ -9,6 +9,15 @@ def jogar():
   acertou = False
 
   while(not enforcou and not acertou):
+
+    chute = input("Qual letra? ")
+
+    index = 0
+
+    for letra in palavra_secreta:
+      if(chute == letra):
+        print("Encontrei a letra{} na posição {}".format(letra, index))
+      index = index + 1
     print("jogando ...")
 
   print("Fim do jogo")
