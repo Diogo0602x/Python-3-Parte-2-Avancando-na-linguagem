75f21db1a87a64cc49541e1b870b271b7cf17470
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-09 08:26:39 -0300

    Aula 06 - Atividade 8 terminada

--- forca.py
+++ forca.py
@@ -1,9 +1,22 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
@@ -34,7 +47,6 @@ def jogar():
         print("Você ganhou!!")
     else:
         print("Você perdeu!!")
-
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
