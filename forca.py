1d0f124db7c85d0261bc368523a198d439d16945
Diogo0602x <diogoteixeira0602@gmail.com>
2021-06-09 08:37:49 -0300

    Aula 07 - Atividade 1 terminada

--- forca.py
+++ forca.py
@@ -1,23 +1,12 @@
 import random
 
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
+    imprime_mensagem_abertura()
 
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
+    palavra_secreta = carrega_palavra_secreta()
 
-    arquivo.close()
-
-    numero = random.randrange(0,len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -48,6 +37,28 @@ def jogar():
     else:
         print("Você perdeu!!")
     print("Fim do jogo")
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()    
+    return palavra_secreta    
 
 if(__name__ == "__main__"):
     jogar()
